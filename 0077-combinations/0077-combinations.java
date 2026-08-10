bf8bf1d958ab5391734a300f73ce0b3b300c6bac
karmanya08 <karmanya0811@gmail.com>
2026-08-10 17:23:46 +0530

    Time: 22 ms (17.83%), Space: 97.8 MB (83.62%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public void backtrack(int start, int n, int k, List<Integer> list,
+                          List<List<Integer>> answer) {
+
+        if (list.size() == k) {
+            answer.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            list.add(i);
+
+            backtrack(i + 1, n, k, list, answer);
+
+            list.remove(list.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+
+        backtrack(1, n, k, list, answer);
+
+        return answer;
+    }
+}
